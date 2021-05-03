33bfb2ff161c267b79acc804e3cb60cb47138aa0
Vtchoo <vittor-limma@hotmail.com>
2021-05-02 22:26:31 -0300

    Format app

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
 		>
 			<Panorama
 				options={{
-				position: { lat: 46.9171876, lng: 17.8951832 },
-				addressControl: false,
-				showRoadLabels: false,
+					position: { lat: 46.9171876, lng: 17.8951832 },
+					addressControl: false,
+					showRoadLabels: false,
 				}}
 				style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center' }}
 			>
