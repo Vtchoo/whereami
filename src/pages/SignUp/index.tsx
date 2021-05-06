6aa7a431d43b4ecdd687a76ded0c2120eec43f51
Vtchoo <vittor-limma@hotmail.com>
2021-05-06 10:54:21 -0300

    Error messages

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -9,7 +9,10 @@ function SignUp() {
 
     const [user, setUser] = useState({} as IUser)
 
+    const [usernameIsValid, setUsernameIsValid] = useState<boolean>()
     const [usernameMessage, setUsernameMessage] = useState('')
+
+    const [emailMessage, setEmailMessage] = useState('')
 
     const [confirmPassword, setConfirmPassword] = useState<string>()
     const [confirmPasswordMessage, setConfirmPasswordMessage] = useState('')
@@ -26,12 +29,18 @@ function SignUp() {
     }, 1500, [user.username])
 
     async function verifyUsername() {
-        if(!user.username) return
-        
-        try {
-            const result = await User.verify(user.username)
-            console.log(result)
+        if (!user.username) {
+            setUsernameIsValid(undefined)
+            setUsernameMessage('')
+            return
+        }
 
+        try {
+            const { count } = await User.verify(user.username)
+            
+            setUsernameIsValid(count === 0)
+            setUsernameMessage(count ? `Username ${user.username} is already taken` : `Username ${user.username} is available`)
+            
         } catch (error) {
             console.log(error)
         }
@@ -47,30 +56,31 @@ function SignUp() {
                     </div>
 
                     <div className={style.inputs}>
-                        <div className={style.inputContainer}>
+                        <div className={style.inputContainer} style={{ borderColor: usernameIsValid === false ? 'red' : usernameIsValid ? 'green' : undefined }}>
                             <Icon icon='user' size='1.5rem' color='grey'/>
-                            <input value={user.username} placeholder='Username' className={style.input} onChange={e => changeUser({ username: e.target.value })}/>
+                            <input value={user.username || ''} placeholder='Username' className={style.input} onChange={e => changeUser({ username: e.target.value })}/>
                         </div>
-                        <span>{usernameMessage}</span>
+                        <small>{usernameMessage}</small>
                         <div className={style.inputContainer}>
                             <Icon icon='at' size='1.5rem' color='grey'/>
-                            <input value={user.email} placeholder='Email' className={style.input} />
+                            <input value={user.email || ''} placeholder='Email' className={style.input} />
                         </div>
+                        <small>{emailMessage}</small>
                         <div className={style.inputContainer}>
                             <Icon icon='key-variant' size='1.5rem' color='grey'/>
-                            <input value={user.password} placeholder='Password' className={style.input} type='password' />
+                            <input value={user.password || ''} placeholder='Password' className={style.input} type='password' />
                         </div>
                         <div className={style.inputContainer}>
                             <Icon icon='key-variant' size='1.5rem' color='grey'/>
                             <input placeholder='Confirm your password' className={style.input} type='password' />
                         </div>
-                        <span>{confirmPasswordMessage}</span>
+                        <small>{confirmPasswordMessage}</small>
                     </div>
 
                     <span>{message}</span>
 
                     <div className={style.buttons}>
-                        <button className={`${style.button} ${style.login}`}>Login</button>
+                        <button className={`${style.button} ${style.login}`}>Create account</button>
                         <Link to='/login' className={`${style.button} ${style.signup}`}>
                             Back to login
                         </Link>
