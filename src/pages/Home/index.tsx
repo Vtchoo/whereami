e46aac5f3a84b8befe83014e81aed35aca015bff
Vtchoo <vittor-limma@hotmail.com>
2021-05-06 23:50:19 -0300

    Implement challenge creation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { IPage } from ".."
 import { useGoogleMaps } from "../../contexts/GoogleMaps"
 import { StreetViewLocation } from "../../contexts/GoogleMaps/types"
+import { Challenge, IChallenge } from "../../models/Challenge"
+import { ILocation } from "../../models/Location"
 
 function Home() {
 
@@ -13,9 +15,35 @@ function Home() {
 
     async function handleGetRandomPanorama() {
         
-        for (let i = 0; i < 5; i++) {
-            const panorama = await getRandomPanorama()
-            setPanoramas(panoramas => [...panoramas, panorama.location])
+        try {
+            // const locations: ILocation[] = []
+            // for (let i = 0; i < 5; i++) {
+            //     const { location, ...panorama } = await getRandomPanorama()
+
+            //     console.log('panorama found:', { location, ...panorama })
+
+            //     locations.push({
+            //         pano: location.pano,
+            //         lat: typeof location.latLng.lat === 'function' ? location.latLng.lat() : location.latLng.lat,
+            //         lng: typeof location.latLng.lng === 'function' ? location.latLng.lng() : location.latLng.lng,
+            //         description: location.description,
+            //         shortdescription: location.description
+            //     })
+            //     // setPanoramas(panoramas => [...panoramas, panorama.location])
+            // }
+    
+            const challenge: IChallenge = {
+                locations: 5,
+                time: 3 * 60 * 1000,
+            }
+
+            alert('sending challenge data')
+
+            const result = await Challenge.create(challenge)
+            
+            console.log(result)
+        } catch (error) {
+            console.log(error)
         }
     }
 
@@ -31,7 +59,7 @@ function Home() {
             <h3>home</h3>
             <h3>home</h3>
             <h3>home</h3>
-            <button onClick={handleGetRandomPanorama}>Get random panoramas</button>
+            <button onClick={handleGetRandomPanorama}>Create challenge</button>
             {panoramas.map((panorama, i) =>
                 <div key={i}>
                     {panorama.pano}
